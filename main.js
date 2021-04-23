84b3daef1e3e34828afb52a9ae978066865cfff8
EC2 Default User <ec2-user@ip-172-31-60-235.ec2.internal>
2021-04-23 06:15:02 +0000

    電卓

---
+++ main.js
@@ -0,0 +1,36 @@
+let myTotal = 0;
+let myInput = "";
+let myCalc = "+";
+let myFlg = 1;
+
+
+
+function myValue(myData){
+  myFlg = 0;
+  myInput += myData;
+  document.myForm.myLine.value = myInput;
+}
+
+
+function myCalculate(myData){
+  if(myFlg == 0){
+    myFlg = 1;
+    myWork = myTotal + myCalc + myInput;
+    myTotal = eval(myWork);
+    myInput = "";
+    document.myForm.myLine.value = myTotal;
+  }
+  if(myData == "="){
+    myTotal = 0;
+    myCalc = "+";
+  }else{
+    myCalc = myData;
+  }
+}
+
+function myC(){
+  myTotal = 0;
+  myCalc = "+";
+  myInput = "";
+  document.myForm.myLine.value = myTotal;
+}
